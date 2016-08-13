731e374af1898d90c8beca34c7f22426794cd76e
user <user@localhost.localdomain>
2016-08-13 18:05:22 +0300

    Add getRandom helper functions

--- source/assets/js/app/simon.js
+++ source/assets/js/app/simon.js
@@ -1,9 +1,30 @@
 "use strict";
 
 var Simon = {
+
     init() {
         console.log("To be implemented.");
+    },
+
+    // Helper functions
+    /**
+     * Returns a random number between 0 (inclusive) and 1 (exclusive)
+     * @returns {Number} Float number
+     */
+    getRandom() {
+    	return Math.random();
     }
+
+    /**
+     * Returns a random integer between min (included) and max (excluded)
+     * @param {Number} min - Non-negative integer
+     * @param {Number} max - Positive integer
+     * @returns {Number} Non-negative integer
+     */
+    getRandomInt(min, max) {
+    	return Math.floor( (getRandom() * max - min) ) + min;
+    }
+
 };
 
 Simon.init();
