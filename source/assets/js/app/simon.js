8f791d2e55d089ed22d636337632ec9d6c9579c5
Andrew Kud <kud@mm.st>
2016-08-14 19:15:28 +0300

    Add helper functions

--- source/assets/js/app/simon.js
+++ source/assets/js/app/simon.js
@@ -1,9 +1,55 @@
-"use strict";
+//"use strict";
 
 var Simon = {
 
+    gameStarted: false,
+    round: 0,
+    buttonsToLight: [],
+
     init() {
         console.log("To be implemented.");
+    },
+
+    /**
+     * Appends random numbers to buttonsToLight ArrayBuffer
+     */
+    generateButtonSequence() {
+
+    	this.resetButtonsToLight();
+
+		for (let i = 0; i <= this.round; i += 1) {
+			this.buttonsToLight.push( this.getRandomInt(0, 3) );
+		}
+    },
+
+    /**
+     * Returns current round number.
+     * @returns {Number} - Round number
+     */
+    getRound() {
+        return this.round;
+    },
+
+    /**
+     * Increments 'round' property as game progresses
+     */
+    incrementRound() {
+    	let currRound = this.getRound();
+    	this.round = currRound + 1;
+    },
+
+    /**
+     * Resets round to default
+     */
+    resetRound() {
+    	this.round = 0;
+    },
+
+    /**
+     * Resets buttonsToLight array.
+     */
+    resetButtonsToLight() {
+    	this.buttonsToLight.length = 0;
     },
 
     // Helper functions
@@ -13,7 +59,7 @@ var Simon = {
      */
     getRandom() {
     	return Math.random();
-    }
+    },
 
     /**
      * Returns a random integer between min (included) and max (excluded)
@@ -22,9 +68,15 @@ var Simon = {
      * @returns {Number} Non-negative integer
      */
     getRandomInt(min, max) {
-    	return Math.floor( (getRandom() * max - min) ) + min;
+    	return Math.floor( (this.getRandom() * max - min) ) + min;
     }
 
 };
 
-Simon.init();
+/*Simon.init();
+Simon.incrementRound();
+Simon.incrementRound();
+Simon.incrementRound();
+Simon.incrementRound();
+Simon.incrementRound();
+Simon.generateButtonSequence();*/
