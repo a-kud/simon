012f75227be482e93897b506dcc2adf93806674d
Andrew Kud <kud@mm.st>
2016-08-28 04:20:58 +0300

    Add AI round turn

--- source/assets/js/app/simon.js
+++ source/assets/js/app/simon.js
@@ -1,81 +1,176 @@
-//"use strict";
+// use strict;
 
-var Simon = {
+let startBtn = document.querySelector("#start"),
+    strictBtn = document.querySelector("#strict"),
+    sw = document.querySelector(".switch");
 
-    gameStarted: false,
-    round: 0,
-    buttonsToLight: [],
+let colorButtons = {
+    0: "red",
+    1: "blue",
+    2: "green",
+    3: "yellow"
+},
+    audioButtons = {
+        0: "assets/sound/simonSound1.mp3",
+        1: "assets/sound/simonSound2.mp3",
+        2: "assets/sound/simonSound3.mp3",
+        3: "assets/sound/simonSound4.mp3"
+    };
 
-    init() {
-        console.log("To be implemented.");
-    },
+let gameStarted = false,
+    round = 2;
 
-    /**
-     * Appends random numbers to buttonsToLight ArrayBuffer
-     */
-    generateButtonSequence() {
+sw.addEventListener("click", function swtichToggled() {
+    console.log("on/off switch pressed")
+    init();
+});
+function handler() {
+    activateButtons();
+}
+function addCtrlBtnsListeners() {
+    startBtn.addEventListener( "click", handler );
+    strictBtn.addEventListener( "click", handler );
+}
 
-        this.resetButtonsToLight();
+function rmCtrlBtnsListeners() {
+    startBtn.removeEventListener( "click", handler );
+    strictBtn.removeEventListener( "click", handler );
+}
 
-		for (let i = 0; i <= this.round; i += 1) {
-			this.buttonsToLight.push( this.getRandomInt(0, 3) );
-		}
-    },
 
-    /**
-     * Returns current round number.
-     * @returns {Number} - Round number
-     */
-    getRound() {
-        return this.round;
-    },
-
-    /**
-     * Increments 'round' property as game progresses
-     */
-    incrementRound() {
-        let currRound = this.getRound();
-        this.round = currRound + 1;
-    },
-
-    /**
-     * Resets round to default
-     */
-    resetRound() {
-        this.round = 0;
-    },
-
-    /**
-     * Resets buttonsToLight array.
-     */
-    resetButtonsToLight() {
-        this.buttonsToLight.length = 0;
-    },
-
-    // Helper functions
-    /**
-     * Returns a random number between 0 (inclusive) and 1 (exclusive)
-     * @returns {Number} Float number
-     */
-    getRandom() {
-        return Math.random();
-    },
-
-    /**
-     * Returns a random integer between min (included) and max (excluded)
-     * @param {Number} min - Non-negative integer
-     * @param {Number} max - Positive integer
-     * @returns {Number} Non-negative integer
-     */
-    getRandomInt(min, max) {
-        return Math.floor( (this.getRandom() * max - min) ) + min;
+function init() {
+    if (!gameStarted) {
+        gameStarted = true;
+        addCtrlBtnsListeners();
+    } else {
+        gameStarted = false;
+        resetRound();
+        rmCtrlBtnsListeners();
     }
-};
 
-Simon.init();
-/*Simon.incrementRound();
-Simon.incrementRound();
-Simon.incrementRound();
-Simon.incrementRound();
-Simon.incrementRound();
-Simon.generateButtonSequence();*/
+}
+
+function activateButtons() {
+    /*1. toggle activated class
+    2. play corresponding sound for button*/
+    generateButtonSequence().then( (arr) => {
+
+        let promises = [];
+        //
+        // for (let num of arr) {
+        //     promises.push(function() {
+        //         lightBtn(colorButtons[num]);
+        //         return playSound(audioButtons[num]).then( () => { offBtnLight(colorButtons[num]); });
+        //     });
+        //     console.log("Current sequence: ", colorButtons[num])
+        // }
+        //
+        arr.forEach( (num) => {
+            promises.push(function() {
+                    lightBtn(colorButtons[num]);
+                    return playSound(audioButtons[num]).then( () => { offBtnLight(colorButtons[num]); });
+                });
+            console.log("Current sequence: ", colorButtons[num])
+        });
+        return promises;
+
+
+    }).then( (promises) => {
+
+        let  result = Promise.resolve();
+        promises.forEach(function (prom) {
+            result = result.then(prom);
+
+            });
+        return result;
+    });
+}
+
+function waitForUserTurn() {
+
+}
+
+/**
+ * Changes backround of color button to idicate activated item
+ */
+function toggleBtnLight(btn) {
+    let element = document.querySelector("." + btn)
+    element.classList.toggle("activated-" + btn);
+}
+
+function lightBtn(btn) {
+    let element = document.querySelector("." + btn)
+    element.classList.add("activated-" + btn);
+}
+
+function offBtnLight(btn) {
+    let element = document.querySelector("." + btn)
+    element.classList.remove("activated-" + btn);
+}
+
+/** Plays specified audiofile
+ * @param {String} path - Path to the file
+ */
+function playSound(path) {
+    return new Promise(function playPromise(resolve, reject) {
+        let audio = new Audio(path);
+        audio.play();
+        audio.onended = resolve;
+        audio.onerror = reject;
+    })
+}
+/**
+ * Returns an array of color button as integers
+ * @returns {ArrayBuffer} buttonsToLight
+ */
+function generateButtonSequence() {
+
+    let buttonsToLight = [];
+
+	for (let i = 0; i <= round; i += 1) {
+		buttonsToLight.push( getRandomInt(0, 4) );
+	}
+    return Promise.resolve(buttonsToLight);
+}
+
+/**
+ * Returns current round number.
+ * @returns {Number} - Round number
+ */
+function getRound() {
+    return round;
+}
+
+/**
+ * Increments round count as game progresses
+ */
+function incrementRound() {
+    let currRound = getRound();
+    round = currRound + 1;
+}
+
+/**
+ * Resets round to default
+ */
+function resetRound() {
+    round = 0;
+}
+
+// Helper functions
+/**
+ * Returns a random number between 0 (inclusive) and 1 (exclusive)
+ * @returns {Number} Float number
+ */
+function getRandom() {
+    return Math.random();
+}
+
+/**
+ * Returns a random integer between min (included) and max (excluded)
+ * @param {Number} min - Non-negative integer
+ * @param {Number} max - Positive integer
+ * @returns {Number} Non-negative integer
+ */
+function getRandomInt(min, max) {
+    return Math.floor( (getRandom() * max - min) ) + min;
+}
